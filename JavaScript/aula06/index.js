ba2e42f32e6509245407c9a4b0bf3f7bfd2dcfa6
valbock <111398539+valbock@users.noreply.github.com>
2022-09-06 20:18:02 -0300

    a

---
+++ JavaScript/aula06/index.js
@@ -0,0 +1,16 @@
+//Não pode criar variaveis com palavras reservadas
+//Variaveis precisam ter nomes significativos
+let nomeCliente='João';
+//Não pode começar com numeros
+//Não podem conter espaços ou traços
+//Utilizar camelCase
+let nomeCompletoDoCliente = 'José da Silva'
+//Variaveis são case-sensitive
+
+let nome = 'João';
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com',nome, 'em 2015.');
+console.log('O filho de',nome, 'se chama Eduardo.')

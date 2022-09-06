5611d6c1ada25b2aa74af9180fdcccc710881d61
valbock <111398539+valbock@users.noreply.github.com>
2022-09-06 20:35:04 -0300

    a

---
+++ JavaScript/aula07/index.js
@@ -0,0 +1,10 @@
+//Não pode criar constantes com palavras reservadas
+//Constantes precisam ter nomes significativos
+//Não pode começar com numeros
+//Não podem conter espaços ou traços
+//Utilizar camelCase
+//Constantes são case-sensitive
+//Não pode modificar o valor de uma constante
+
+const nome = 'João';
+console.log(nome)

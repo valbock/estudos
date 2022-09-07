c0b30f06cf895abd86063e3feb6702db8d7ebe4d
valbock <111398539+valbock@users.noreply.github.com>
2022-09-07 10:37:43 -0300

    a

---
+++ JavaScript/aula10/index.js
@@ -0,0 +1,14 @@
+// dados primitivos 
+const nome = 'Valter'; //String
+const nome2 = "Valter"; //String
+const nome3 = `Valter`; //String
+const num1 = 10; // number
+const num2 = 10.59; // number
+let nomeAluno; // undefined indefinida, só inicia 
+let sobrenomeAluno = null; // Nulo 
+let aprovado = true; // Boolean true ou false
+
+console.log(typeof nome);
+console.log(typeof num1);
+console.log(typeof aprovado);
+
---
+++ JavaScript/aula11/index.js
@@ -0,0 +1,16 @@
+/**
+ * Operadores aritiméticos
+ * + adição e concatenação
+ * - subtração
+ * * multiplicação
+ * / divisão
+ * ** potenciação
+ * % resto da divisão
+ * 
+ * 
+ */
+ const num1 = 5;
+ const num2 = 10;
+ console.log(Math.floor(num1/num2));
+ console.log(num1/num2);
+

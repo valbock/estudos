0cdac7275c48b5d482e1a4ddb5329eeb6f91499b
valbock <valbock@gmail.com>
2022-09-21 17:45:47 -0300

    0

--- JavaScript/_secao4/aula50/aula.js
+++ JavaScript/_secao4/aula50/aula.js
@@ -1,7 +1,7 @@
-// passando arguments sem o argumento na função.
-// somente nas funções declaradas com function, arrow funcions não funciona.
-// a variavel arguments recebe todos os argumentos passados na chamada da função.
-// mesmo passando paramentos.
+// // passando arguments sem o argumento na função.
+// // somente nas funções declaradas com function, arrow funcions não funciona.
+// // a variavel arguments recebe todos os argumentos passados na chamada da função.
+// // mesmo passando paramentos.
 // function funcao(a, b, c) {
 //   console.log('Oi!');
 //   console.log(arguments);
@@ -20,7 +20,33 @@
 // }
 // funcao(0,1);
 
-function funcao(a, b = 2, c = 4) {
-  console.log(a + b + c);
-}
-funcao(2,undefined,10);
+// function funcao(a, b = 2, c = 4) {
+//   console.log(a + b + c);
+// }
+// funcao(2,undefined,10);
+
+// function funcao({nome, sobrenome, idade}) {
+//   console.log(nome, sobrenome, idade);
+// }
+
+// // funcao({nome: 'José', sobrenome: 'da Silva', idade:30});
+// // ou...
+// let obj = {nome: 'José', sobrenome: 'da Silva', idade:30};
+// funcao(obj);
+
+// function conta(operador, acumulador, ...numeros){
+//   for (let numero of numeros) {
+//     if (operador === '+') acumulador += numero;
+//     if (operador === '-') acumulador -= numero;
+//     if (operador === '/') acumulador /= numero;
+//     if (operador === '*') acumulador *= numero;
+//   }
+//   console.log(acumulador);
+// }
+
+// conta('*', 10, 1,2,3,4);
+
+// Rest parameters ...
+const conta = (...args) => { console.log(args);};
+
+conta('*', 10, 1,2,3,4);

e3e99e7a91b3c0ecc83d41b540873fe8659896a0
valbock <111398539+valbock@users.noreply.github.com>
2022-09-22 22:16:45 -0300

    124

---
+++ JavaScript/_secao4/aula57/assets/js/main.js
@@ -0,0 +1,25 @@
+function criaCalculadora() {
+  return {
+    display: document.querySelector('display'),
+
+    
+    
+    
+    
+    
+    inicia() {
+      alert('oi, iniciei')
+    },
+    cliqueBotoes() {
+      document.addEventListener('click', function(e) {
+        const el = e.target;
+        if (el.classList.contains('btn-num')) {
+          this.btnParaDisplay();
+        }
+      });
+    },
+  }
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();

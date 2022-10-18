f8adb95ca63fddf11547d76955c22aeb6af0535f
valbock <111398539+valbock@users.noreply.github.com>
2022-10-18 07:09:27 -0300

    ok

---
+++ JavaScript/_secao4/aula66/aula66.js
@@ -0,0 +1,38 @@
+// map 
+
+//Dobre os números
+//               0  1   2   3  4 ...                      12
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+// const numerosEmDobro = numeros.map(function(valor, indice, array) {
+//   // console.log(valor, indice, array);
+//   return valor*2
+// })
+
+// const numerosEmDobro = numeros.map(valor => valor*2);
+// console.log(numerosEmDobro);
+
+// Para cada elemento
+// Retorne apenas uma string com o nome da pessoa
+// Remova apenas a chave "nome" do objeto
+// Adicone uma chave "id" em cada objeto
+
+const pessoas = [
+  {nome: 'Luiz', idade: 62},
+  {nome: 'Maria', idade: 23},
+  {nome: 'Eduardo', idade: 55},
+  {nome: 'Letícia', idade: 19},
+  {nome: 'Rosana', idade: 32},
+  {nome: 'Wallace', idade: 47},
+];
+
+// const nomes = pessoas.map(valor => valor.nome);
+// console.log(nomes);
+
+// const idades = pessoas.map(obj => ({idade: obj.idade}));
+// console.log(idades);
+
+const comIds = pessoas.map()
+
+tempo 14:54
+

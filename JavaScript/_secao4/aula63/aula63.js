e813d4a4fdff28ff47941c025aae1f8f9a0aa463
valbock <valbock@gmail.com>
2022-10-19 16:58:41 -0300

    ok

--- JavaScript/_secao4/aula63/aula63.js
+++ JavaScript/_secao4/aula63/aula63.js
@@ -3,6 +3,16 @@ const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
 
 // nomes.splice(indice, delete, elem1, elem2, elem3); //elemx são adicionados
 
+//const numeroMaximo = Number.MAX_VALUE;
+
+
+// simular o pop()
+// const removidos = nomes.splice(3, 2); // mesmo que o abaixo
+// const removidos = nomes.splice(-2, 2); // mesmo que o acima
+// const removidos = nomes.splice(-2, 1000); // pode-se usar qualquer numero final 
+// const removidos = nomes.splice(-2, numeroMaximo); // numero máximo do javaScript 
+//console.log(numeroMaximo);
+
 // pop
 // const removido = nomes.splice(-1, 1);
 // const removido = nomes.splice(-2, Number.MAX_VALUE);
